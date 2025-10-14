fe799780521fd290813db3ccd95e750467902db7
Andrii Umanets <umanets93@gmail.com>
2025-10-14 22:06:21 +0200

    logic separation

---
+++ bot_scripts.js
@@ -0,0 +1,5 @@
+function start_script (name) {
+  return `Hi, ${name}, I am Car Insurance Sales bot! A'll be happy to assist you with car insurance purchases!`;
+}
+
+module.exports = { start_script };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Telegraf } = require("telegraf");
 const express = require("express");
+const { start_script } = require("./bot_scripts");
 require("dotenv/config");
 
 const token = process.env.BOT_TOKEN;
@@ -10,9 +11,9 @@ if (!token) {
 
 const bot = new Telegraf(token);
 
-bot.start((ctx) => ctx.reply(`Hi, ${ctx.from.first_name || "friend"}!`));
+bot.start((ctx) => ctx.reply(start_script(ctx.from.first_name)));
 bot.help((ctx) => ctx.reply("Type something"));
-bot.on('text', ctx => ctx.reply(`Ты написал: ${ctx.message.text}`));
+bot.on("text", (ctx) => ctx.reply(`Ты написал: ${ctx.message.text}`));
 
 bot
   .launch()
@@ -21,10 +22,10 @@ bot
     console.error("Failed to lounch bot: ", err);
   });
 
-const app = express()
+const app = express();
 app.get("/", (req, res) => res.send("Bot is working!"));
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server is listening port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is listening port ${PORT}`));
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
