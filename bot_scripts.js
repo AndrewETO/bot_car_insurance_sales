695714b559df9263241c98511f518f5698725b60
Andrii Umanets <umanets93@gmail.com>
2025-10-15 00:13:02 +0200

    troubleshooting

--- bot_scripts.js
+++ bot_scripts.js
@@ -2,11 +2,7 @@ function start_script (name) {
   return (
     `Hi, ${name}, I am Car Insurance bot! 
     A'll be happy to assist you with car insurance purchasing!
-    Would you like to proceed further?`,
-    Markup.keyboard([
-        ['Proceed']
-    ])
-    .resize()
+    Would you like to proceed further?`
 );
 }
 
